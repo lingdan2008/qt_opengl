99c93468af59c0bdffbb0450d2a88987768c738d
lingdan2008 <lingdan_2008@163.com>
2020-01-23 21:07:56 +0800

    add 3D model

--- openglwidget.cpp
+++ openglwidget.cpp
@@ -31,12 +31,33 @@ void OpenGLWidget::paintGL()
     glRotatef(m_rtri, 0.0f, 1.0f, 0.0f);
 
     glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 0.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f);                    //红色
+    glVertex3f(0.0f, 1.0f, 0.0f);                   //上顶点(前侧面)
+    glColor3f(0.0f, 1.0f, 0.0f);                    //绿色
+    glVertex3f(-1.0f, -1.0f, 1.0f);                 //左下(前侧面)
+    glColor3f(0.0f, 0.0f, 1.0f);                    //蓝色
+    glVertex3f(1.0f, -1.0f, 1.0f);                  //右下(前侧面)
+
+    glColor3f(1.0f, 0.0f, 0.0f);                    //红色
+    glVertex3f(0.0f, 1.0f, 0.0f);                   //上顶点(右侧面)
+    glColor3f(0.0f, 0.0f, 1.0f);                    //蓝色
+    glVertex3f(1.0f, -1.0f, 1.0f);                  //左下(右侧面)
+    glColor3f(0.0f, 1.0f, 0.0f);                    //绿色
+    glVertex3f(1.0f, -1.0f, -1.0f);                 //右下(右侧面)
+
+    glColor3f(1.0f, 0.0f, 0.0f);                    //红色
+    glVertex3f(0.0f, 1.0f, 0.0f);                   //上顶点(后侧面)
+    glColor3f(0.0f, 1.0f, 0.0f);                    //绿色
+    glVertex3f(1.0f, -1.0f, -1.0f);                 //左下(后侧面)
+    glColor3f(0.0f, 0.0f, 1.0f);                    //蓝色
+    glVertex3f(-1.0f, -1.0f, -1.0f);                //右下(后侧面)
+
+    glColor3f(1.0f, 0.0f, 0.0f);                    //红色
+    glVertex3f(0.0f, 1.0f, 0.0f);                   //上顶点(左侧面)
+    glColor3f(0.0f, 0.0f, 1.0f);                    //蓝色
+    glVertex3f(-1.0f, -1.0f, -1.0f);                //左下(左侧面)
+    glColor3f(0.0f, 1.0f, 0.0f);                    //绿色
+    glVertex3f(-1.0f, -1.0f, 1.0f);                 //右下(左侧面)
     glEnd();
 
     glLoadIdentity();
@@ -46,10 +67,41 @@ void OpenGLWidget::paintGL()
     glRotatef(m_rquad, 1.0f, 0.0f, 0.0f);
     glColor3f(0.5f, 0.5f, 1.0f);
     glBegin(GL_QUADS);
-    glVertex3f(-1.0f, 1.0f, 0.0f);
-    glVertex3f(1.0f, 1.0f, 0.0f);
-    glVertex3f(1.0f, -1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);                    //绿色
+    glVertex3f(1.0f, 1.0f, -1.0f);                  //右上(顶面)
+    glVertex3f(-1.0f, 1.0f, -1.0f);                 //左上(顶面)
+    glVertex3f(-1.0f, 1.0f, 1.0f);                  //左下(顶面)
+    glVertex3f(1.0f, 1.0f, 1.0f);                   //右下(顶面)
+
+    glColor3f(1.0f, 0.5f, 0.0f);                    //橙色
+    glVertex3f(1.0f, -1.0f, 1.0f);                  //右上(底面)
+    glVertex3f(-1.0f, -1.0f, 1.0f);                 //左上(底面)
+    glVertex3f(-1.0f, -1.0f, -1.0f);                //左下(底面)
+    glVertex3f(1.0f, -1.0f, -1.0f);                 //右下(底面)
+
+    glColor3f(1.0f, 0.0f, 0.0f);                    //红色
+    glVertex3f(1.0f, 1.0f, 1.0f);                   //右上(前面)
+    glVertex3f(-1.0f, 1.0f, 1.0f);                  //左上(前面)
+    glVertex3f(-1.0f, -1.0f, 1.0f);                 //左下(前面)
+    glVertex3f(1.0f, -1.0f, 1.0f);                  //右下(前面)
+
+    glColor3f(1.0f, 1.0f, 0.0f);                    //黄色
+    glVertex3f(1.0f, -1.0f, -1.0f);                 //右上(后面)
+    glVertex3f(-1.0f, -1.0f, -1.0f);                //左上(后面)
+    glVertex3f(-1.0f, 1.0f, -1.0f);                 //左下(后面)
+    glVertex3f(1.0f, 1.0f, -1.0f);                  //右下(后面)
+
+    glColor3f(0.0f, 0.0f, 1.0f);                    //蓝色
+    glVertex3f(-1.0f, 1.0f, 1.0f);                  //右上(左面)
+    glVertex3f(-1.0f, 1.0f, -1.0f);                 //左上(左面)
+    glVertex3f(-1.0f, -1.0f, -1.0f);                //左下(左面)
+    glVertex3f(-1.0f, -1.0f, 1.0f);                 //右下(左面)
+
+    glColor3f(1.0f, 0.0f, 1.0f);                    //紫色
+    glVertex3f(1.0f, 1.0f, -1.0f);                  //右上(右面)
+    glVertex3f(1.0f, 1.0f, 1.0f);                   //左上(右面)
+    glVertex3f(1.0f, -1.0f, 1.0f);                  //左下(右面)
+    glVertex3f(1.0f, -1.0f, -1.0f);                 //右下(右面)
     glEnd();
 
     m_rtri += 0.5f;
